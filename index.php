72c34fc230b46779d471911650bacbb258af6005
sanil <sanilmanandhar@gmail.com>
2020-08-22 10:56:45 +0545

    index

--- index.php
+++ index.php
@@ -3,10 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <!-- Bootstrap 4 CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Homepage</title>
 </head>
 
 <body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 offset-md-4 form-div login">
+                <div class="alert alert-success">
+                    You are now logged in!
+                </div>
+                <h3>Welcome, Sanil</h3>
+
+                <a href="#" class="logout">logout</a>
+
+                <div class="alert alert-warning">
+                    You need to verify your account.
+                    Sign in to your email account and click on the
+                    verification link we just emailed you at
+                    <strong>sanilmanandhar@gmail.com</strong>
+                </div>
+
+                <button class="btn btn-block btn-lg btn-primary">I am verified!</button>
+            </div>
+        </div>
+    </div>
 
 </body>
 
