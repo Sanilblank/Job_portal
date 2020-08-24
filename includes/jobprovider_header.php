a429517272a8f77ca8d02750934b4ecfa0b1a48c
sanil <sanilmanandhar@gmail.com>
2020-08-24 13:48:49 +0545

    constructed sidebar

---
+++ includes/jobprovider_header.php
@@ -0,0 +1,33 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job Proviider</title>
+    <link rel="stylesheet" href="headerStyle.css">
+    <script src="https://kit.fontawesome.com/37572b58fa.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <input type="checkbox" id="check">
+    <label for="check">
+        <i class="fas fa-bars" id="btn"></i>
+        <i class="fas fa-times" id="cancel"></i>
+    </label>
+    <div class="sidebar">
+        <header>My App</header>
+        <ul>
+            <li><a href="#"><i class="fab fa-accessible-icon"></i>Dashboard</a></li>
+            <li><a href="#"><i class="fab fa-accessible-icon"></i>Shortcuts</a></li>
+            <li><a href="#"><i class="fab fa-accessible-icon"></i>Overview</a></li>
+            <li><a href="#"><i class="fab fa-accessible-icon"></i>Events</a></li>
+            <li><a href="#"><i class="fab fa-accessible-icon"></i>About</a></li>
+            <li><a href="#"><i class="fab fa-accessible-icon"></i>Services</a></li>
+            <li><a href="#"><i class="fab fa-accessible-icon"></i>Contact</a></li>
+        </ul>
+    </div>
+    <section></section>
+
+</body>
+
+</html>
